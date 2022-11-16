2dabc38c69f93a37d46ed5c6c81350e9425e3ae7
Harshal Patel <harshalpateljnv@gmail.com>
2022-11-16 23:55:19 +0530

    multiplication simple function

---
+++ multiFun.py
@@ -0,0 +1,10 @@
+def multi(num1: int, num2: int) -> int:
+    """multiplication of two numbers"""
+    num3 = num1 * num2
+ 
+    return num3
+ 
+# Driver code
+num1, num2 = 5, 15
+ans = multi(num1, num2)
+print(f"The multiplication of {num1} and {num2} results {ans}.")
